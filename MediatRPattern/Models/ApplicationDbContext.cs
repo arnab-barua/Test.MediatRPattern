695de50294f5fb3f9b7fc39b210b1d6e71073f2c
Arnab Barua <arnabjitu@gmail.com>
2020-07-19 14:37:27 +0600

    Initialize project

---
+++ MediatRPattern/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MediatRPattern.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ MediatRPattern/Startup.cs
@@ -0,0 +1,49 @@
+using MediatRPattern.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MediatRPattern
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var connection = Configuration.GetConnectionString("DatabaseConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+        }
+
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
